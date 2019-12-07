0a0f751a141c05c735e106ea66098d698a0d6ecf
bomber <tangyihong063@163.com>
2019-12-07 20:05:06 +0800

    重构tabs组件的部分代码，让其不超过五行代码

--- src/tabs.vue
+++ src/tabs.vue
@@ -33,11 +33,36 @@
                 eventBus:this.eventBus
             }
         },
-        mounted(){
-            if(this.$children.length===0){
-                // throw new Error('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
+        methods:{
+            checkChildren(){
+                if(this.$children.length===0){
+                    // throw new Error('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
                     console&&console.warn&&console.warn('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
+                }
+            },
+            selectTab(){
+                this.$children.forEach((vm)=>{
+                    //第一次循环，因为有两个子组件，一个是head一个是body
+                    if (vm.$options.name === 'GuluTabsHead') {
+                        // console.log('第一次循环')
+                        // console.log(vm)
+                        vm.$children.forEach((childVm) => {
+                            //第二次循环，因为有三个子组件都是item,但是name不一样。然后找到它的options里面的name和它props里面的name就找到对应的子组件了props可以省略。比如item.name
+                            if (childVm.name === this.selected && childVm.$options.name === 'GuluTabsItem') {
+                                // console.log('第二次循环')
+                                // console.log(childVm.name)
+                                // console.log(childVm.$el.getBoundingClientRect())
+                                // console.log(childVm.$el)
+                                this.eventBus.$emit('update:selected', this.selected, childVm)
+                            }
+                        })
+                    }
+                })
+                // this.eventBus.$emit('update:selected',this.selected,this)
             }
+        },
+        mounted(){
+            this.checkChildren();
             // console.log('eventBus')
             // console.log(this.eventBus)
             // console.log('this')
@@ -45,24 +70,8 @@
                 // this.$emit('update:selected','xxx')
                 // this.$emit('update:selected','我是this.$emit触发的事件出来的数据')
                 //下面是初始选中的selected
-            this.$children.forEach((vm)=>{
-                //第一次循环，因为有两个子组件，一个是head一个是body
-                if (vm.$options.name === 'GuluTabsHead') {
-                    // console.log('第一次循环')
-                    // console.log(vm)
-                    vm.$children.forEach((childVm) => {
-                        //第二次循环，因为有三个子组件都是item,但是name不一样。然后找到它的options里面的name和它props里面的name就找到对应的子组件了props可以省略。比如item.name
-                        if (childVm.name === this.selected && childVm.$options.name === 'GuluTabsItem') {
-                            // console.log('第二次循环')
-                            // console.log(childVm.name)
-                            // console.log(childVm.$el.getBoundingClientRect())
-                            // console.log(childVm.$el)
-                            this.eventBus.$emit('update:selected', this.selected, childVm)
-                        }
-                    })
-                }
-            })
-                // this.eventBus.$emit('update:selected',this.selected,this)
+            this.selectTab()
+
         }
     }
 </script>
